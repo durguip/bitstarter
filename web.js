76cc7c2f563559c1a15c8dc1781b1c6d2dec5e03
Raquel Montagut <rmontagut@gmail.com>
2013-07-19 21:46:35 +0200

    The images of index.html were not found

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
-  var fs = require('fs');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use('/images', express.static("./images"));
+
 app.get('/', function(request, response) {
-
-  var data = fs.readFileSync('./index.html', 'utf8');
-  response.send(data.toString());
-
+	var data = fs.readFileSync('./index.html', 'utf8');
+ 	response.send(data.toString());
 });
 
 var port = process.env.PORT || 8080;
